813555c98c475cc10b276420c168646efa5a0d8e
migs911 <migs1488@gmail.com>
2015-09-16 00:09:56 +0300

    переключение между снимками + кнопка Назад скрыта вначале

--- desktop.blocks/next/next.js
+++ desktop.blocks/next/next.js
@@ -3,9 +3,23 @@ modules.define('next', ['i-bem__dom'], function(provide, BEMDOM) {
         onSetMod: {
             'js': {
                 'inited': function() {
-                    this.bindTo('click', function() { alert('Next please!'); });
+                    this.bindTo('click', function() {
+						selectedImage.src = selectedImage.src.replace(/(.*gallery_images\/)(\d*)(\.jpg)/, nextImagePath);
+						nextPicture.style.display = isLastPicture() ? 'none' : 'block';
+						previousPicture.style.display = isFirstPicture() ? 'none' : 'block';
+					});
                 }
             }
         }
     }));
 }); 
+
+// !TODO на серверной стороне генерировать упорядоченный список 
+// картинок, а не работать с названием файла
+function isLastPicture(){
+	return selectedImage.src.search('/21.jpg') == -1 ? false : true;
+}
+
+function nextImagePath(str, dir, filename, ext, offset, s) {
+	return dir + ++filename + ext;
+}
--- desktop.blocks/previous/previous.js
+++ desktop.blocks/previous/previous.js
@@ -3,9 +3,23 @@ modules.define('previous', ['i-bem__dom'], function(provide, BEMDOM) {
         onSetMod: {
             'js': {
                 'inited': function() {
-                    this.bindTo('click', function() { alert('Previous please!'); });
+                    this.bindTo('click', function() {
+						selectedImage.src = selectedImage.src.replace(/(.*gallery_images\/)(\d*)(\.jpg)/, previousImagePath);
+						previousPicture.style.display = isFirstPicture() ? 'none' : 'block';
+						nextPicture.style.display = isLastPicture() ? 'none' : 'block';
+					});
                 }
             }
         }
     }));
 }); 
+
+// !TODO на серверной стороне генерировать упорядоченный список 
+// картинок, а не работать с названием файла
+function isFirstPicture(){
+	return selectedImage.src.search('/1.jpg') == -1 ? false : true;
+}
+
+function previousImagePath(str, dir, filename, ext, offset, s) {
+	return dir + --filename + ext;
+}
